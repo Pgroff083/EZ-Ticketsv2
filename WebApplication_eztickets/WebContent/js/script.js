70cf619355b71039f7b5598be72879f8b6a49ece
anlili022 <zcchen0228@gmail.com>
2018-03-07 18:05:16 -0600

    manage the folder and fix linking problems and add poster pics

--- WebApplication_eztickets/WebContent/js/script.js
+++ WebApplication_eztickets/WebContent/js/script.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-  
-	// The next line is a call to the ratings.js file that handles the rating system
-	var r = new SimpleStarRating(document.getElementById('rating_1'));
-	
+
   $(".ratting-star").click(function() {
     this.replaceWith('star');
   });
